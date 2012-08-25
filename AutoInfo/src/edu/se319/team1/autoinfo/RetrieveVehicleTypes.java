2e47e142bfcfa3281139ef04d432d8dc07875436
Brian Reber <reber.brian@gmail.com>
2012-08-25 14:36:07 -0500

    Keep track of how many records added/updated
    Also, add some exception handling

--- AutoInfo/src/edu/se319/team1/autoinfo/RetrieveVehicleTypes.java
+++ AutoInfo/src/edu/se319/team1/autoinfo/RetrieveVehicleTypes.java
@@ -89,6 +89,11 @@ public class RetrieveVehicleTypes extends HttpServlet {
 			e.printStackTrace();
 		}
 
+		// Keep track of how many entities were added, and how
+		// many were updated
+		int numAdded = 0;
+		int numUpdated = 0;
+
 		// Go through the list and add new entries, and update times
 		// of pre-exisiting entries
 		try {
@@ -103,34 +108,45 @@ public class RetrieveVehicleTypes extends HttpServlet {
 
 				q.setFilter(CompositeFilterOperator.and(make, model, year));
 
+				Transaction txn = datastore.beginTransaction();
 				try {
 					// Prepare the query. Try and get the result as a single entity.
 					//  - If the result is null, we don't have a record, so we want to add it
 					//  - If the pq.asSingleEntity method throws a TooManyResultsException, just log an error
-					Transaction txn = datastore.beginTransaction();
 					PreparedQuery pq = datastore.prepare(q);
 					Entity result = pq.asSingleEntity();
 
 					// If result isn't null, update the last modified time to the current time
 					if (result != null) {
+						numUpdated++;
 						result.setProperty(Vehicle.VehicleColumns.LAST_MODIFIED, currentTime);
 					} else {
 						// If the result is null, we want to add the vehicle to the database
 						pm.makePersistent(v);
-					}
-
-					if (txn.isActive()) {
-						txn.commit();
+						numAdded++;
 					}
 				} catch (TooManyResultsException ex) {
 					log.log(Level.WARNING, "More than one result...");
 					log.log(Level.WARNING, v.toString());
+				} finally {
+					if (txn.isActive()) {
+						txn.commit();
+					}
 				}
 			}
+		} catch (Exception e) {
+			log.log(Level.WARNING, "Error modifiying database");
+			log.log(Level.WARNING, e.getMessage());
+			log.log(Level.WARNING, Arrays.toString(e.getStackTrace()));
 		} finally {
 			pm.close();
 		}
 
 		// TODO: query database for records with old lastmodified times and possibly delete them...
+
+		// Print out status report
+		log.log(Level.FINE, "Finished " + RetrieveVehicleTypes.class.getSimpleName());
+		log.log(Level.FINE, "Num Updated:  " + numUpdated);
+		log.log(Level.FINE, "Num Added:  " + numAdded);
 	}
 }

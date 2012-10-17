a1e13bae765c9fd78796360362333f921b42b9f6
Brian Reber <reber.brian@gmail.com>
2012-10-17 15:54:49 -0500

    Add some of the admin pages

---
+++ Carhub/admin.py
@@ -0,0 +1,31 @@
+from google.appengine.api import memcache, users
+from google.appengine.ext.webapp import template
+import os
+import utils
+import webapp2
+
+class AdminHandler(webapp2.RequestHandler):
+    def get(self, method):
+        if users.is_current_user_admin():
+            context = utils.get_context()
+            
+            path = os.path.join(os.path.dirname(__file__), 'templates/admin.html')
+            self.response.out.write(template.render(path, context))
+
+    def post(self, method):
+        if method:
+            # Do something...
+            if method == "fetchvehicleinfo":
+                temp = "temp"
+                # TODO: fetch vehicle inforomation from cars.com
+            elif method == "deletevehicles":
+                temp = "temp"
+                # TODO: delete all vehicles
+            elif method == "deletecarresponsestring":
+                temp = "temp"
+                # TODO: delete car response string
+            elif method == "clearmemcache":
+                memcache.Client().flush_all()
+        
+        # Always redirect to admin
+        self.redirect("/admin")

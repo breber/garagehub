4e35a15732565f23969374c75568922e86aeb8cf
Brian Reber <reber.brian@gmail.com>
2012-12-05 17:18:13 -0600

    Update all pages to use dataTables bootstrap
    Also update some spacing on maintenance page

--- Carhub/static/js/maintenance.js
+++ Carhub/static/js/maintenance.js
@@ -2,48 +2,47 @@ var editID;
 var maintDatatables;
 
 $(document).ready(function() {
-	var h3s = new Array();
+	var h3s = [];
 	$('.h3').each(function(i) {
 		h3s[i] = this.innerHTML;
-	} );
-	
-	$('.maint-datatable').dataTable( {
-		"sDom": "<'row'<'span6'l><'span6'f>r>t",
-		"bPaginate": false,
-		"bLengthChange": false,
-		"bFilter": false,
-		"bSort": true,
-		"bInfo": false,
-		"bAutoWidth": false,
-		"aaSorting": [[ 0, 'desc' ]]
-	} );
-	
-	//make rows selectable
+	});
+
+	$('.maint-datatable').dataTable({
+		"sDom" : "t",
+		"bPaginate" : false,
+		"bLengthChange" : false,
+		"bFilter" : false,
+		"bSort" : true,
+		"bInfo" : false,
+		"bAutoWidth" : false,
+		"aaSorting" : [ [ 0, 'desc' ] ]
+	});
+
+	// make rows selectable
 	$('.maint-datatable tbody').click(function(event) {
-		$($('.maint-datatable').dataTable().fnSettings().aoData).each(function (){
+		$($('.maint-datatable').dataTable().fnSettings().aoData).each(function() {
 			$(this.nTr).removeClass('active');
 		});
+
 		$(event.target.parentNode).addClass('active');
-		
-		//get key for record
+
+		// get key for record
 		editID = event.target.parentNode.id;
 	});
 
-	$('.receiptlink').click( function() {
+	$('.receiptlink').click(function() {
 		$('#displayimage').modal();
 		$('#modalimage').attr('src', this.getAttribute('value'));
 	});
-
 });
 
-function editRecord(link){
+function editRecord(link) {
 	// make sure something is selected
-	if(editID) {
-		window.location = link + editID;		
-	} else if(link.indexOf("edit") > -1){
+	if (editID) {
+		window.location = link + editID;
+	} else if (link.indexOf("edit") > -1) {
 		alert("Please select a record to edit.");
-	} else if(link.indexOf("delete") > -1){
+	} else if (link.indexOf("delete") > -1) {
 		alert("Please select a record to delete.");
 	}
-	
 }

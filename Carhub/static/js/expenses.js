dc84ef396399fd15a26ea062bbf9ec27297d1f74
Brian Reber <reber.brian@gmail.com>
2012-12-05 19:54:45 -0600

    Temporarily set img width to 400

--- Carhub/static/js/expenses.js
+++ Carhub/static/js/expenses.js
@@ -9,42 +9,41 @@ $(document).ready(function() {
 		"bSort" : true,
 		"bInfo" : false,
 		"bAutoWidth" : false,
-		"oTableTools": {
-            "sRowSelect": "multi",
+		"oTableTools" : {
+			"sRowSelect" : "multi",
 		},
 		/*"aoColumnDefs": [
 		                 { "bVisible":    false, "aTargets": [0]} 
-		             ],*/  /* this code will hide the first row  */
+		             ],*//* this code will hide the first row  */
 		"aaSorting" : [ [ 0, 'desc' ] ]
 	});
-	 
+
 	//make rows selectable
 	$('#expense-table tbody').click(function(event) {
-		$(dTable.fnSettings().aoData).each(function (){
+		$(dTable.fnSettings().aoData).each(function() {
 			$(this.nTr).removeClass('active');
 		});
 		$(event.target.parentNode).addClass('active');
-		
+
 		//get key for record
 		editID = event.target.parentNode.id;
 	});
 });
 
-$('.receiptlink').click( function() {
+$('.receiptlink').click(function() {
 	$('#displayimage').modal();
 	$('#modalimage').attr('src', this.getAttribute('value'));
 });
 
-function editRecord(link){
+function editRecord(link) {
 	// make sure something is selected
-	if(editID) {
-		window.location = link + editID;		
-	} else if(link.indexOf("edit") > -1){
+	if (editID) {
+		window.location = link + editID;
+	} else if (link.indexOf("edit") > -1) {
 		alert("Please select a record to edit.");
-	} else if(link.indexOf("delete") > -1){
+	} else if (link.indexOf("delete") > -1) {
 		alert("Please select a record to delete.");
 	}
-	
 }
 
 //TODO

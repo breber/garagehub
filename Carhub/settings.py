7b94c872ad681053a5e95bd4301fae36b416e318
Brian Reber <reber.brian@gmail.com>
2012-12-12 17:58:58 -0600

    Update settings page logic

--- Carhub/settings.py
+++ Carhub/settings.py
@@ -13,69 +13,65 @@ class SettingsHandler(webapp2.RequestHandler):
         context = utils.get_context()
         user = users.get_current_user()
         
-        logging.warn("pageName = %s, pageType = %s" % (pageName, pageType))
+        if action == "delete":
+            # Delete record
+            category = datastore.getCategoryById(user.user_id(), categoryId)
+            if category:
+                category.key.delete()
+            else:
+                logging.warn("No category object was deleted. Couldn't find it.")
+
+            self.redirect("/settings")
+            return
         
-        if user:
-            if action == "delete":
-                # Delete record
-                category = datastore.getCategoryById(user.user_id(), categoryId)
-                if category:
-                    category.key.delete()
-                else:
-                    logging.warn("No category object was deleted. Couldn't find it.")
-    
-                self.redirect("/settings")
-                return
-            
-            # go to regular settings page
-            context["categories"] = datastore.getExpenseCategoryModels(user.user_id(), False)
-            context["defaultcategories"] = datastore.getDefaultExpenseCategoryModels()
-            context["maintcategories"] = datastore.getMaintenanceCategoryModels(user.user_id(), False)
-            context["defaultmaintcategories"] = datastore.getDefaultMaintenanceCategoryModels()
-            path = os.path.join(os.path.dirname(__file__), 'templates/settings.html')
-            self.response.out.write(template.render(path, context))
-       
-        else:
-            self.redirect("/")
+        # go to regular settings page
+        context["categories"] = datastore.getExpenseCategoryModels(user.user_id(), False)
+        context["defaultcategories"] = datastore.getDefaultExpenseCategoryModels()
+        context["maintcategories"] = datastore.getMaintenanceCategoryModels(user.user_id(), False)
+        context["defaultmaintcategories"] = datastore.getDefaultMaintenanceCategoryModels()
+        path = os.path.join(os.path.dirname(__file__), 'templates/settings.html')
+        self.response.out.write(template.render(path, context))
+
     def post(self, pageName, pageType, action, categoryId):
         user = users.get_current_user()
-        if user:
-            if action == "add":
+
+        if action == "add":
+            if pageType == "maintenance":
+                categories = datastore.getMaintenanceCategoryStrings(user.user_id())
+            else:
+                categories = datastore.getExpenseCategoryStrings(user.user_id())
+                
+            newName = self.request.get("categoryName", None)
+            if newName and not newName in categories:
+                # this is a new category, add it to the database
+                newCategoryObj = models.ExpenseCategory()
                 if pageType == "maintenance":
-                    categories = datastore.getMaintenanceCategoryStrings(user.user_id())
+                    newCategoryObj.category = "Maintenance"
+                    newCategoryObj.subcategory = newName
                 else:
-                    categories = datastore.getExpenseCategoryStrings(user.user_id())
-                    
-                newName = self.request.get("categoryName", None)
-                if newName and not newName in categories:
-                    # this is a new category, add it to the database
-                    if pageType == "maintenance":
-                        newCategoryObj = models.ExpenseCategory()
-                        newCategoryObj.category = "Maintenance"
-                        newCategoryObj.subcategory = newName
-                    else:
-                        newCategoryObj = models.ExpenseCategory()
-                        newCategoryObj.category = newName
-                    newCategoryObj.owner = user.user_id()
-                    newCategoryObj.put()
-            
-            if action == "edit":
-                # Edit record
-                if pageType == "maintenance":
-                    categories = datastore.getMaintenanceCategoryStrings(user.user_id())
-                else:
-                    categories = datastore.getExpenseCategoryStrings(user.user_id())
-                    
-                newName = self.request.get("categoryName", None)
-                category = datastore.getCategoryById(user.user_id(), pageType, categoryId)
-                newName = self.request.get("categoryName", None)
-                logging.warn("New Name %s" % newName)
-                if category and newName and not newName in categories:
-                    category.category = newName
-                    category.put()
-                else:
-                    #TODO: need a way to give user feedback about why the record was not edited
-                    logging.warn("No category object was edited. Couldn't find it. Or you can't rename it to a duplicate name")
+                    newCategoryObj.category = newName
+
+                newCategoryObj.owner = user.user_id()
+                newCategoryObj.put()
+        
+        if action == "edit":
+            # Edit record
+            if pageType == "maintenance":
+                categories = datastore.getMaintenanceCategoryStrings(user.user_id())
+            else:
+                categories = datastore.getExpenseCategoryStrings(user.user_id())
+                
+            newName = self.request.get("categoryName", None)
+            category = datastore.getCategoryById(user.user_id(), pageType, categoryId)
+
+            logging.warn("New Name %s" % newName)
+            if category and newName and not newName in categories:
+                # TODO: should this be category/subcategory based on whether it is a maintenance record?
+                category.category = newName
+                category.put()
+            else:
+                #TODO: need a way to give user feedback about why the record was not edited
+                logging.warn("No category object was edited. Couldn't find it. Or you can't rename it to a duplicate name")
     
         self.redirect("/settings")
                 

6a9e68f7f05acd847492e8a6368b335658311836
Brian Reber <reber.brian@gmail.com>
2013-02-24 10:28:36 -0600

    Sort fuel table by odometer reading

--- Carhub/static/js/gasmileage.js
+++ Carhub/static/js/gasmileage.js
@@ -1,3 +1,47 @@
+jQuery.fn.dataTableExt.oSort['number-range-asc']  = function(a,b) {
+	var aEnd;
+	var bEnd;
+
+	a = a.replace(/\s+/g, " ");
+	b = b.replace(/\s+/g, " ");
+	
+	if (/- (.+)/.test(a)) {
+		aEnd = RegExp.$1;
+	}
+	
+	if (/- (.+)/.test(b)) {
+		bEnd = RegExp.$1;
+	}
+	
+	var x = (a == "-") ? 0 : aEnd.replace( /,/, "." );
+	var y = (b == "-") ? 0 : bEnd.replace( /,/, "." );
+	x = parseFloat(x);
+	y = parseFloat(y);
+	return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+};
+
+jQuery.fn.dataTableExt.oSort['number-range-desc'] = function(a,b) {
+	var aEnd;
+	var bEnd;
+
+	a = a.replace(/\s+/g, " ");
+	b = b.replace(/\s+/g, " ");
+
+	if (/- (.+)/.test(a)) {
+		aEnd = RegExp.$1;
+	}
+	
+	if (/- (.+)/.test(b)) {
+		bEnd = RegExp.$1;
+	}
+	
+	var x = (a == "-") ? 0 : aEnd.replace( /,/, "." );
+	var y = (b == "-") ? 0 : bEnd.replace( /,/, "." );
+	x = parseFloat(x);
+	y = parseFloat(y);
+	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+};
+
 $(document).ready(function() {
 	var dTable = $('#gasmileagetable').dataTable({
 		"sDom" : "t",
@@ -7,7 +51,17 @@ $(document).ready(function() {
 		"bSort" : true,
 		"bInfo" : false,
 		"bAutoWidth" : false,
-		"aaSorting" : [ [ 0, 'desc' ] ]
+		"aaSorting" : [ [ 6, 'desc' ] ],
+		"aoColumns": [
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			{ "sType": "number-range" },
+			null
+		]
 	});
 
 	// Setup the handlers for the edit/delete buttons

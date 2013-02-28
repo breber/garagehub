b65044ea4cd1998be7b88f972ef1028e0905cd2c
Brian Reber <reber.brian@gmail.com>
2013-02-28 17:27:05 -0600

    Fix gasmileage sorting

--- Carhub/static/js/gasmileage.js
+++ Carhub/static/js/gasmileage.js
@@ -1,83 +1,76 @@
-jQuery.fn.dataTableExt.oSort['number-range-asc']  = function(a,b) {
-	var aEnd;
-	var bEnd;
+jQuery.fn.dataTableExt.oSort['number-range-asc'] = function(a, b) {
+    var aEnd;
+    var bEnd;
 
-	a = a.replace(/\s+/g, " ");
-	b = b.replace(/\s+/g, " ");
-	
-	if (/- (.+)/.test(a)) {
-		aEnd = RegExp.$1;
-	}
-	
-	if (/- (.+)/.test(b)) {
-		bEnd = RegExp.$1;
-	}
-	
-	var x = (a == "-") ? 0 : aEnd.replace( /,/, "." );
-	var y = (b == "-") ? 0 : bEnd.replace( /,/, "." );
-	x = parseFloat(x);
-	y = parseFloat(y);
-	return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+    a = a.replace(/\s+/g, " ");
+    b = b.replace(/\s+/g, " ");
+
+    if (/- (.+)/.test(a)) {
+        aEnd = RegExp.$1;
+    }
+
+    if (/- (.+)/.test(b)) {
+        bEnd = RegExp.$1;
+    }
+
+    var x = (a == "-") ? 0 : aEnd.replace(",", "");
+    var y = (b == "-") ? 0 : bEnd.replace(",", "");
+    x = parseFloat(x);
+    y = parseFloat(y);
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
 };
 
-jQuery.fn.dataTableExt.oSort['number-range-desc'] = function(a,b) {
-	var aEnd;
-	var bEnd;
+jQuery.fn.dataTableExt.oSort['number-range-desc'] = function(a, b) {
+    var aEnd;
+    var bEnd;
 
-	a = a.replace(/\s+/g, " ");
-	b = b.replace(/\s+/g, " ");
+    a = a.replace(/\s+/g, " ");
+    b = b.replace(/\s+/g, " ");
 
-	if (/- (.+)/.test(a)) {
-		aEnd = RegExp.$1;
-	}
-	
-	if (/- (.+)/.test(b)) {
-		bEnd = RegExp.$1;
-	}
-	
-	var x = (a == "-") ? 0 : aEnd.replace( /,/, "." );
-	var y = (b == "-") ? 0 : bEnd.replace( /,/, "." );
-	x = parseFloat(x);
-	y = parseFloat(y);
-	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+    if (/- (.+)/.test(a)) {
+        aEnd = RegExp.$1;
+    }
+
+    if (/- (.+)/.test(b)) {
+        bEnd = RegExp.$1;
+    }
+
+    var x = (a == "-") ? 0 : aEnd.replace(",", "");
+    var y = (b == "-") ? 0 : bEnd.replace(",", "");
+    x = parseFloat(x);
+    y = parseFloat(y);
+    return ((x < y) ? 1 : ((x > y) ? -1 : 0));
 };
 
 $(document).ready(function() {
-	var dTable = $('#gasmileagetable').dataTable({
-		"sDom" : "t",
-		"bPaginate" : false,
-		"bLengthChange" : false,
-		"bFilter" : false,
-		"bSort" : true,
-		"bInfo" : false,
-		"bAutoWidth" : false,
-		"aaSorting" : [ [ 6, 'desc' ] ],
-		"aoColumns": [
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			{ "sType": "number-range" },
-			null
-		]
-	});
+    var dTable = $('#gasmileagetable').dataTable({
+        "sDom" : "t",
+        "bPaginate" : false,
+        "bLengthChange" : false,
+        "bFilter" : false,
+        "bSort" : true,
+        "bInfo" : false,
+        "bAutoWidth" : false,
+        "aaSorting" : [ [ 6, 'desc' ] ],
+        "aoColumns" : [ null, null, null, null, null, null, {
+            "sType" : "number-range"
+        }, null ]
+    });
 
-	// Setup the handlers for the edit/delete buttons
-	setupHandlers();
+    // Setup the handlers for the edit/delete buttons
+    setupHandlers();
 
-	// make rows selectable
-	$('#gasmileagetable tbody').click(function(event) {
-		$(dTable.fnSettings().aoData).each(function() {
-			$(this.nTr).removeClass('active');
-		});
+    // make rows selectable
+    $('#gasmileagetable tbody').click(function(event) {
+        $(dTable.fnSettings().aoData).each(function() {
+            $(this.nTr).removeClass('active');
+        });
 
-		$(event.target.parentNode).addClass('active');
-	});
+        $(event.target.parentNode).addClass('active');
+    });
 
-	$('.receiptlink').click(function() {
-		$('#displayimage').modal();
-		$('#modalimage').attr('src', this.getAttribute('value'));
-	});
+    $('.receiptlink').click(function() {
+        $('#displayimage').modal();
+        $('#modalimage').attr('src', this.getAttribute('value'));
+    });
 });

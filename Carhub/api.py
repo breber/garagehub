500167ac801b92c5a030e5da84524ef8dda5ab64
Brian Reber <reber.brian@gmail.com>
2013-06-18 21:56:35 -0500

    Update the API

--- Carhub/api.py
+++ Carhub/api.py
@@ -1,18 +1,22 @@
 from google.appengine.ext import endpoints
 from protorpc import remote
 from models import *
+import logging
 
-CLIENT_ID = '280486107933.apps.googleusercontent.com'
+ANDROID_CLIENT_ID = '280486107933.apps.googleusercontent.com'
+ANDROID_DEBUG_CLIENT_ID = '280486107933-4r3v4nis34qimv27bc3pb8vppk38nvho.apps.googleusercontent.com'
+CLIENT_ID = '280486107933-fkp13pk6dv84vdkumqu1vj5hh0o74he3.apps.googleusercontent.com'
 
 @endpoints.api(name='carhub',version='v1',
                description='CarHub API',
                hostname='car-hub.appspot.com',
-               audiences=[CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID],
-               allowed_client_ids=[CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID])
+               audiences=[CLIENT_ID, ANDROID_CLIENT_ID, ANDROID_DEBUG_CLIENT_ID],
+               allowed_client_ids=[ANDROID_CLIENT_ID, ANDROID_DEBUG_CLIENT_ID, CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID])
 class CarHubApi(remote.Service):
 
-    @UserVehicle.query_method(path='vehicles', name='vehicles.list')
+    @UserVehicle.query_method(user_required=True, path='vehicles', name='vehicles.list')
     def VehiclesList(self, query):
-        return query
+        logging.warn("User: %s" % endpoints.get_current_user())
+        return query.filter(UserVehicle.owner == endpoints.get_current_user().user_id())
 
 application = endpoints.api_server([CarHubApi], restricted=False)

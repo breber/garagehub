2d5f3524b39a0e263835023fda6cd940a48f290c
Brian Reber <reber.brian@gmail.com>
2013-06-28 22:44:59 -0500

    Fix some things with the script

--- generateendpoints.sh
+++ generateendpoints.sh
@@ -1,4 +1,4 @@
-CARHUB_MOBILE=../CarHubMobile/CarHubMobile
+CARHUB_MOBILE=$(pwd)/../CarHubMobile/CarHubMobile
 LIB_VERSION=1.15.0-rc
 
 # Generate the Java client library
@@ -6,27 +6,34 @@ endpointscfg.py get_client_lib java -o . -f rest api.CarHubApi
 
 # Unzip the generated zip
 mv CarHubApi.zip /tmp/CarHubApi.zip
-cd /tmp/
-unzip CarHubApi.zip
+pushd /tmp/
+yes | unzip CarHubApi.zip
+pushd carhub
 
 # Remove existing Jars from the libs folder of Android app
-rm $CARHUB_MOBILE/CarHub/libs/google-*.jar
-rm $CARHUB_MOBILE/CarHub/libs/gson-*.jar
-rm $CARHUB_MOBILE/CarHub/libs/jsr-*.jar
+rm $CARHUB_MOBILE/CarHub/libs/google-*
+rm $CARHUB_MOBILE/CarHub/libs/gson-*
+rm $CARHUB_MOBILE/CarHub/libs/jsr*
 
 # Copy new jars to the Android app
-cp carhub/libs/gson-*.jar $CARHUB_MOBILE/CarHub/libs/
-cp carhub/libs/jsr-*.jar $CARHUB_MOBILE/CarHub/libs/
-cp carhub/libs/google-api-client-*.jar $CARHUB_MOBILE/CarHub/libs/
-cp carhub/libs/google-api-client-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
-cp carhub/libs/google-api-client-android-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
-cp carhub/libs/google-http-client-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
-cp carhub/libs/google-http-client-android-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
-cp carhub/libs/google-http-client-gson-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
-cp carhub/libs/google-oauth-client-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
+cp libs/gson-*.jar $CARHUB_MOBILE/CarHub/libs/
+cp libs/jsr*.jar $CARHUB_MOBILE/CarHub/libs/
+cp libs/google-api-client-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
+cp libs/google-api-client-android-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
+cp libs/google-http-client-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
+cp libs/google-http-client-android-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
+cp libs/google-http-client-gson-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
+cp libs/google-oauth-client-$LIB_VERSION.jar $CARHUB_MOBILE/CarHub/libs/
 
 # Get the java files out of the generated Jar
-mv car-hub-carhub-*-java-$LIB_VERSION-sources.jar car-hub-carhub.zip
+mv car-hub-carhub*.jar car-hub-carhub.zip
 unzip car-hub-carhub.zip
 
-cp -R car-hub-carhub-v1/com/google/* $CARHUB_MOBILE/CarHub/src/main/java/com/google/
+cp -R com/google/* $CARHUB_MOBILE/CarHub/src/main/java/com/google/
+
+popd # carhub
+popd # /tmp/
+
+# Clean up after ourselves
+rm /tmp/CarHubApi.zip
+rm -rf /tmp/carhub

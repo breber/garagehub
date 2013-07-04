f21e7d24f178c182217e58a2680faf0c60ff8f25
Brian Reber <reber.brian@gmail.com>
2013-07-03 20:21:44 -0500

    Remove the "Fuel Up" category on base expense page
    You shouldn't be able to add a fuel record from a generic add expense
    page, because there is a lot of necessary information missing for a
    fuel record.
    
    Fixes issue #15

--- static/js/addexpense.js
+++ static/js/addexpense.js
@@ -4,12 +4,12 @@ var categoriesLength = -1;
 $(document).ready(function() {
 	// For adding records: display input fields based on which record is being added
 	if (window.location.pathname.indexOf("add") > -1) {
-		if(window.location.pathname.indexOf("gasmileage") > -1){
-			$('.gasmileage').removeClass( "hidden");
-		} else if(window.location.pathname.indexOf("maintenance") > -1){
-			$('.maintenance').removeClass( "hidden");
-		} else if(window.location.pathname.indexOf("expense") > -1){
-			$('.generic').removeClass( "hidden");
+		if (window.location.pathname.indexOf("gasmileage") > -1) {
+			$('.gasmileage').removeClass("hidden");
+		} else if (window.location.pathname.indexOf("maintenance") > -1) {
+			$('.maintenance').removeClass("hidden");
+		} else if (window.location.pathname.indexOf("expense") > -1) {
+			$('.generic').removeClass("hidden");
 		}
 	}
 
@@ -17,7 +17,7 @@ $(document).ready(function() {
 	$('#addform').validationEngine();
 
 	var currentDate = new Date();
-	var prettyDate = (currentDate.getFullYear() + '/' + (currentDate.getMonth()+1) + '/' + currentDate.getDate());
+	var prettyDate = (currentDate.getFullYear() + '/' + (currentDate.getMonth() + 1) + '/' + currentDate.getDate());
 	// make the date picker work
 	$('#datePurchased').datepicker({
 		format: 'yyyy/mm/dd'
@@ -25,17 +25,28 @@ $(document).ready(function() {
 
 	$('#datePurchased').val(prettyDate);
 
+    // Remove "Fuel Up" from categories - make the user go to the designated fuel up page
+    var index = -1;
+    $.each($("#category option"), function (e, value) {
+        if (value.innerHTML === "Fuel Up") {
+            index = e;
+        }
+    });
+    if (index > 0) {
+        var category = document.getElementById("category");
+        category.remove(index);
+    }
 
 	// make function to toggle between using new category and existing categories
-	$('#categoryButton').click(function(){
+	$('#categoryButton').click(function() {
 		// hide validation warnings because you are not submitting it
 		$('#addform').validationEngine('hide');
 
-		if($('#newCategory').val() != "" && $('#newCategory').val() != null && $('#newCategory').val() != "Enter New Category"){
-			if(categoriesLength == -1){
+		if ($('#newCategory').val() != "" && $('#newCategory').val() != null && $('#newCategory').val() != "Enter New Category") {
+			if (categoriesLength == -1) {
 				categoriesLength = $('#category')[0].options.length;
 			}
-			$('#category')[0].options[categoriesLength]=new Option($('#newCategory').val(), $('#newCategory').val(), false, true);
+			$('#category')[0].options[categoriesLength] = new Option($('#newCategory').val(), $('#newCategory').val(), false, true);
 
 			// switch to category tab
 			$('#categorytab a:first').tab('show');
@@ -54,13 +65,13 @@ $(document).ready(function() {
 	if (window.location.pathname.indexOf("edit") > -1) {
 		editExpenseRecord();
 		if ($("#editfuelrecordobject").length > 0) {
-			$('.gasmileage').removeClass( "hidden");
+			$('.gasmileage').removeClass("hidden");
 			editFuelRecord();
 		} else if ($("#editmaintenanceobject").length > 0) {
-			$('.maintenance').removeClass( "hidden");
+			$('.maintenance').removeClass("hidden");
 			editMaintenanceRecord();
 		} else if ($("#editexpenseobject").length > 0) {
-			$('.generic').removeClass( "hidden");
+			$('.generic').removeClass("hidden");
 			editExpenseRecord();
 		}
 	}
@@ -68,9 +79,10 @@ $(document).ready(function() {
 
 function newCategoryKeyPress(e) {
 	// look for window.event in case event isn't passed in
-	if (typeof e == 'undefined' && window.event) { e = window.event; }
-	if (e.keyCode == 13)
-	{
+	if (typeof e == 'undefined' && window.event) {
+        e = window.event;
+    }
+	if (e.keyCode == 13) {
 		document.getElementById('categoryButton').click();
 	}
 }
@@ -80,11 +92,11 @@ function editExpenseRecord() {
 	// change header
 	$('.page-header').text("Edit Expense");
 
-	$('#datePurchased').val( $('#editdatepurchased').text());
-	$('#category').val( $('#editcategory').text());
-	$('#location').val( $('#editlocation').text());
+	$('#datePurchased').val($('#editdatepurchased').text());
+	$('#category').val($('#editcategory').text());
+	$('#location').val($('#editlocation').text());
 	$('#amount').val($('#editamount').text());
-	$('#description').val( $('#editdescription').text());
+	$('#description').val($('#editdescription').text());
 }
 
 // Populate Maintenance fields based on record input
@@ -92,12 +104,12 @@ function editMaintenanceRecord() {
 	// change header
 	$('.page-header').text("Edit Maintenance Record");
 
-	$('#datePurchased').val( $('#editdatepurchased').text());
-	$('#category').val( $('#editcategory').text());
-	$('#location').val( $('#editlocation').text());
+	$('#datePurchased').val($('#editdatepurchased').text());
+	$('#category').val($('#editcategory').text());
+	$('#location').val($('#editlocation').text());
 	$('#amount').val($('#editamount').text());
-	$('#description').val( $('#editdescription').text());
-	$('#odometerEnd').val( $('#editodometer').text());
+	$('#description').val($('#editdescription').text());
+	$('#odometerEnd').val($('#editodometer').text());
 }
 
 // Populate Fuel Record fields based on record input
@@ -105,10 +117,10 @@ function editFuelRecord() {
 	// change header
 	$('.page-header').text("Edit Fuel Record");
 
-	$('#datePurchased').val( $('#editdatepurchased').text());
-	$('#location').val( $('#editlocation').text());
+	$('#datePurchased').val($('#editdatepurchased').text());
+	$('#location').val($('#editlocation').text());
 	$('#amount').val($('#editamount').text());
-	$('#description').val( $('#editdescription').text());
+	$('#description').val($('#editdescription').text());
 	$('#pricepergallon').val($('#editpricepergallon').text());
 	$('#grade').val($('#editfuelgrade').text());
 
@@ -121,7 +133,7 @@ function editFuelRecord() {
 		$('#odometerStart').val(tmpOdometer);
 	}
 	tmpOdometer = $('#editodometerend').text();
-	if(tmpOdometer != "-1"){
+	if (tmpOdometer != "-1") {
 		$('#odometerEnd').val(tmpOdometer);
 	}
 }

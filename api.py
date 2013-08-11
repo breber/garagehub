fa03890ef85ec33956b80cdd16a1021349b21ea5
Brian Reber <reber.brian@gmail.com>
2013-08-11 12:40:02 -0500

    Move keys out to private submodule
    Some point in the future, we will update the allowed keys and remove
    these existing keys

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+from carhub_keys import carhubkeys
 from google.appengine.ext import endpoints
 from protorpc import remote
 from models import *
@@ -5,11 +6,6 @@ import auth_util
 import datastore
 import datetime
 import logging
-
-ANDROID_CLIENT_ID = '280486107933.apps.googleusercontent.com'
-ANDROID_DEBUG_CLIENT_ID = '280486107933-4r3v4nis34qimv27bc3pb8vppk38nvho.apps.googleusercontent.com'
-IOS_CLIENT_ID = '280486107933-pas5m2v4p51t44qitn9ql2gh7ba5npcp.apps.googleusercontent.com'
-CLIENT_ID = '280486107933-fkp13pk6dv84vdkumqu1vj5hh0o74he3.apps.googleusercontent.com'
 
 # TODO: this probably isn't the best solution, but it should work for now
 def get_user_by_auth(uid):
@@ -33,8 +29,8 @@ def get_user_by_auth(uid):
 @endpoints.api(name='carhub',version='v1',
                description='CarHub API',
                hostname='car-hub.appspot.com',
-               audiences=[CLIENT_ID, ANDROID_CLIENT_ID, ANDROID_DEBUG_CLIENT_ID],
-               allowed_client_ids=[ANDROID_CLIENT_ID, ANDROID_DEBUG_CLIENT_ID, CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID, IOS_CLIENT_ID])
+               audiences=carhubkeys.AUDIENCES,
+               allowed_client_ids=carhubkeys.ALLOWED_CLIENT_IDS)
 class CarHubApi(remote.Service):
 
     @UserVehicle.query_method(user_required=True,

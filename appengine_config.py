7ffbdd7fab87b70331cc4f2f1ecc20da2c4071d1
Brian Reber <reber.brian@gmail.com>
2017-08-02 19:27:11 -0500

    Update to endpoints v2

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,8 @@
 import os
 import sys
+from google.appengine.ext import vendor
+
+vendor.add('lib')
 
 ENDPOINTS_PROJECT_DIR = os.path.join(os.path.dirname(__file__),
                                      'endpoints-proto-datastore')
